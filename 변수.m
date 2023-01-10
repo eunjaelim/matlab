1ba4a2641b4b1e467a91869d5bcd2dc4b4353922
eunjaelim <imeunjae361@gmail.com>
2023-01-10 15:34:43 +0900

    equation

---
+++ 변수.m
@@ -0,0 +1,27 @@
+a = 3
+a
+b = [-3.1;4.9]
+b
+c=[-2 4.67]
+
+d = [2 3 0;-4 5 6]
+
+a3=5
+a+a3
+area = 2.3
+
+Area =4.56;
+namelengthmax
+
+sin(3*pi)
+sin(3)=2
+sin(3)
+
+clear sin
+sin(3)
+pi
+format long
+pi
+format long e
+pi * 100
+help format
