78103478ab9dc875a6bbff1b3c2c675031dc4fdf
eunjaelim <imeunjae361@gmail.com>
2023-01-30 19:33:31 +0900

    계산기역활로서의 매틀랩

---
+++ calculator.m
@@ -0,0 +1,20 @@
+speed_kps = 300000
+year_sec=365*24*60*60
+Lightyear_km = year_sec*speed_kps
+earth_to_sun_km =150e6
+earth_to_sun_sec = earth_to_sun_km/speed_kps
+earth_to_sun_minutes = earth_to_sun_sec/60
+
+earth_to_moon_km = 3844e10
+earth_to_moon_sec = earth_to_moon_km/speed_kps
+earth_to_moon_minutes = earth_to_moon_sec/60
+
+
+
+Minnie = 1928
+
+
+abs_zero_C = -273.1500
+abs_zero_F = abs_zero_C*9/5 + 32;
+paper_ignition_F = 451;
+x = 42; y = 87;
