f838257def6cff37801a21a655c44f62ac5c90b6
eunjaelim <imeunjae361@gmail.com>
2023-02-02 20:24:08 +0900

    행렬과연산자소개

---
+++ matrix2.m
@@ -0,0 +1,14 @@
+xvalues = [1,2,10]
+% 2X3 행렬
+X = [0 1 -1;2.5 pi 100]
+sqrt(2)
+sin(30)
+size(X)
+x = 5
+size(x)
+Voltages = [1 2 3; 4 5 6; 7 8 9]
+row1_Voltages=[1 2 3]
+x = [1 4 7]
+y = [1;4;7]
+size(x)
+size(y)
